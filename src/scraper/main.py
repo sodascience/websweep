051b84bff055ccc5da90229c225c13c348b0e4b6
Javier Garcia-Bernardo <j.garciabernardo@uu.nl>
2022-04-07 16:27:04 +0200

    semaphore added

--- src/scraper/main.py
+++ src/scraper/main.py
@@ -13,13 +13,15 @@ def classify_url(url, level):
     # Keep the path only (avoid to reject websites such as "awesomeshop.nl/important_information")
     url = urlparse(url).path
     
+    # Maybe if there are many links in one level we can skip it
+
     # Download first level and important sites of the secodn level (level 0 = root website)
     if level == 0:
         return True
     elif level == 1:
         # Cloudfare protection --> reject
         # regex = re.compile( "|".join(map(re.escape, keywords)))
-        regex = re.compile(r'collections|email\-protection|product|aanbod|assortiment|voorraad|koop|shop|artikelen|merken|wintersport|bouw|zoeken|search')
+        regex = re.compile(r'^mailto:|^tel:|pdf$|collections|email\-protection|product|aanbod|assortiment|voorraad|koop|shop|artikelen|merken|wintersport|bouw|zoeken|search')
         if re.search(regex, url):
             return False
         # If only numbers and characters (e.g. https:/www.horstingkilder.nl/553-504") --> reject
@@ -29,7 +31,7 @@ def classify_url(url, level):
             return True
     elif level == 2:
         # Keep only if it seems important
-        regex = re.compile(r'over\-ons|contact|duurzaamheid|index\.php|algemene\-voorwaarden|vacatures|disclaimer|klantenservice|privacy\-policy|cookie\-policy|cookies|cookie|cookie\-beleid|over|overons|blogs|privacyverklaring|about|about\-us')
+        regex = re.compile(r'over\-ons|contact|duurzaamheid|index\.php|al   gemene\-voorwaarden|vacatures|disclaimer|klantenservice|privacy\-policy|cookie\-policy|cookies|cookie|cookie\-beleid|over|overons|blogs|privacyverklaring|about|about\-us')
         if re.search(regex, url):
             return True
         else:
@@ -54,8 +56,8 @@ if __name__ == "__main__":
     print(len(urls))
 
     # Run scraper
-    # Start scraper
-    scraper = Scraper(save_html=True, max_level=3, base_path="data/scraped_data", classifier=classify_url)
+    # Start scraper, downloading 20 companies in parallel
+    scraper = Scraper(save_html=True, max_level=3, base_path="data/scraped_data", classifier=classify_url, concurrency=20)
     scraper.scrape_companies(urls)
 
     #Read what we did
@@ -65,25 +67,4 @@ if __name__ == "__main__":
             if line.split("\t")[4] == "200":
                 count += 1
     print(f"Downloaded {count} pages from {len(urls)} urls to level {3} in {time() - start:2.1f} seconds.")
-
-    # Run the scraper with batchs of 100 urls:
-    if 0:
-        # split urls into batches of 100
-        urls_batches = [urls[i:i+100] for i in range(0, len(urls), 100)]
-
-
-        for urls_batch in urls_batches:
-            # Start scraper
-            scraper = Scraper(save_html=False, max_level=3, base_path="data/scraped_data", classifier=classify_url)
-            scraper.scrape_companies(urls_batch)
-
-
-            #Read what we did
-            with open("data/overview_urls.tsv") as f:
-                count = 0
-                for line in f:
-                    if line.split("\t")[4] == "200":
-                        count += 1
-            print("Batch done")
-            print(f"Downloaded {count} pages from {len(urls_batch)} urls to level {3} in {time() - start:2.1f} seconds.")
 
--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -22,16 +22,21 @@ logger = logging.getLogger()
 #     logger.setLevel(logging.ERROR)
 
 
-
 class Scraper:
-    def __init__(self, save_html=True, max_level=3, base_path="data/scraped_data", classifier=lambda url,level: True, verify_ssl=False):
+    def __init__(self, save_html=True, max_level=3, base_path="data/scraped_data", classifier=lambda url,level: True, verify_ssl=False, concurrency=20):
         self.save_html = save_html
         self.base_path = base_path
         self.max_level = max_level
 
+        self.start = time()
+
         # Error in SSL certificates
         self.verify_ssl = verify_ssl
         self.classifier = classifier
+
+        # Companies processed in parallel
+        self.sem_num_comps = asyncio.Semaphore(concurrency)
+
 
         self.waits = dict()
         self.headers =   {
@@ -68,9 +73,9 @@ class Scraper:
 
         #Create path
         if url[-1] == "/":
-            path = f"{self.base_path}/{kvk}/{urlparse(url).netloc}/{self.__get_current_date()}/{hash_url}_{url[:-1].split('/')[-1]}"
+            path = f"{self.base_path}/{kvk}/{urlparse(url).netloc.replace('www.','')}/{self.__get_current_date()}/{hash_url}_{url[:-1].split('/')[-1]}"
         else:
-            path = f"{self.base_path}/{kvk}/{urlparse(url).netloc}/{self.__get_current_date()}/{hash_url}_{url.split('/')[-1]}"
+            path = f"{self.base_path}/{kvk}/{urlparse(url).netloc.replace('www.','')}/{self.__get_current_date()}/{hash_url}_{url.split('/')[-1]}"
         path = path.replace(" ","_")
 
         try:
@@ -97,16 +102,13 @@ class Scraper:
                     #print(urls)
                     
                     # filter out urls from other domains
-                   
-
                     # create base url
                     url_parsed = urlparse(url)
                     base_url = url_parsed.scheme + "://" + url_parsed.netloc
 
                     # add netloc/schema when missing
                     # TODO: do we want to keep mailto? mailto:info@frieslandbouwdetachering.nl?
-                    urls = [urljoin(base_url, url_found) for url_found in urls if 
-                            ("mailto:" not in url_found) and ("tel:" not in url_found)]
+                    urls = [urljoin(base_url, url_found) for url_found in urls]
 
                     # keep only the urls found within the same domain
                     urls = [url_found for url_found in urls if tldextract.extract(url_found).registered_domain == tldextract.extract(url).registered_domain]
@@ -141,63 +143,63 @@ class Scraper:
         """
         Crawl the website of a company up max_level. Save html to file.
         """
-        start = time()
+        async with self.sem_num_comps:
+            print(f"{url} started at {time()-self.start:2.1f} seconds")
+            start = time()
 
-        # name and url
-        kvk, url = url
+            # name and url
+            kvk, url = url
 
-        self.waits[kvk] = 0
-
-        level = 0
-        all_records = [url]
-        records = [url]
-        
-
-        # Breath first search algorithm from urls
-        while (len(records) > 0) and (level < self.max_level):
-            tasks = []
-
-            # fetch urls asynchroneously
-            for url in records:
-                task = asyncio.ensure_future(self.__fetch_one_url(url, kvk=kvk, level=level))
-                tasks.append(task) 
-
-            records = await asyncio.gather(*tasks) 
-
-            # flatten list python and remove duplicates
-            records = [item for sublist in records  if sublist is not None for item in sublist]
-
-            # speed up search using a set (and remove www to avoid downloading twice the same url)
-            temp_all_records = set([url.replace("www.","") for url in all_records])
-
-            # make sure the scraper doesn't run forever
-            if len(temp_all_records) > 100:
-                logger.warn(f'scraper downloaded over 100 subpages of "{url}"')
-                break
-
-            # remove urls alrady downloaded 
-            records = list(set([url for url in records if url.replace("www.","") not in temp_all_records]))
-
-            # add new urls to list
-            all_records += records
-            level += 1
-
-            # reset waits for next level
             self.waits[kvk] = 0
 
+            level = 0
+            all_records = [url]
+            records = [url]
+            
 
-        logger.info(f'scraper for {all_records[0]} finished in {time()-start:2.0f} seconds with {len(temp_all_records)} processed and {len(all_records)} links found.')
-        #return all_records
+            # Breath first search algorithm from urls
+            while (len(records) > 0) and (level < self.max_level):
+                tasks = []
+
+                # fetch urls asynchroneously
+                for url in records:
+                    task = asyncio.ensure_future(self.__fetch_one_url(url, kvk=kvk, level=level))
+                    tasks.append(task) 
+
+                records = await asyncio.gather(*tasks) 
+
+                # flatten list python and remove duplicates
+                records = [item for sublist in records  if sublist is not None for item in sublist]
+
+                # speed up search using a set (and remove www to avoid downloading twice the same url)
+                temp_all_records = set([url.replace("www.","") for url in all_records])
+
+                # make sure the scraper doesn't run forever
+                if len(temp_all_records) > 100:
+                    logger.warn(f'scraper downloaded over 100 subpages of "{url}"')
+                    break
+
+                # remove urls alrady downloaded 
+                records = list(set([url for url in records if url.replace("www.","") not in temp_all_records]))
+
+                # add new urls to list
+                all_records += records
+                level += 1
+
+                # reset waits for next level
+                self.waits[kvk] = 0
+
+
+            logger.info(f'scraper for {all_records[0]} finished in {time()-start:2.0f} seconds with {len(temp_all_records)} processed and {len(all_records)} links found.')
+            #return all_records
 
     async def __fetch_all(self, records):
         """
         Fetch all urls in records up to a level max_level. Save html to file.
         
         """
-
-        #TODO use QUEue: https://stackoverflow.com/questions/52582685/using-asyncio-queue-for-producer-consumer-flow
         tasks = []
-        async with ClientSession(headers=self.headers, trust_env=True, connector=TCPConnector(ssl=self.verify_ssl)) as self.session:
+        async with ClientSession(headers=self.headers, trust_env=True, connector=TCPConnector(limit=100, ssl=self.verify_ssl)) as self.session:
             for url in records:
                 task = asyncio.ensure_future(self.__fetch_one_company(url))
                 tasks.append(task) 
