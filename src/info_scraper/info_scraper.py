21bb195b4bd0b3541cd835d651e3e42d4566dc04
TonsillarRat6 <caspervanwetten@gmail.com>
2022-04-21 22:49:04 +0200

    Initial file creation

---
+++ src/info_scraper/info_scraper.py
@@ -0,0 +1,25 @@
+import logging
+
+
+logging.basicConfig(filename="logs/info_scraper.log", level=logging.INFO)
+logger = logging.getLogger()
+
+class InfoScraper:
+    def __init__(self):
+
+        # fill this with stuff
+
+    def scrape_address(self):
+        return
+
+    def scrape_phone(self):
+        return
+
+    def scrape_BTW(self):
+        return
+
+    def scrape_email(self):
+        return
+
+    def save_to_csv(self):
+        return
