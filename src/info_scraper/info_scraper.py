6216b5406b3027e49c976c9045136c258643684a
TonsillarRat6 <caspervanwetten@gmail.com>
2022-04-29 11:43:36 +0200

    Improved folder-reading algorithm, started the initial reading of the files, haven't added asynchronity or any form of regex yet

--- src/info_scraper/info_scraper.py
+++ src/info_scraper/info_scraper.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import re
 
 
 # logging.basicConfig(filename="logs/info_scraper.log", level=logging.INFO)
@@ -13,25 +15,28 @@ class InfoScraper:
         self.phone_number = phone_number
         self.working_dir = working_dir
 
-    def run_loops(self, current_working_file):
-        self.scrape_address(current_working_file)
-        self.scrape_phone(current_working_file)
-        self.scrape_BTW(current_working_file)
-        self.scrape_email(current_working_file)
+    def run_loops(self):
+        for file in os.scandir(self.working_dir):
+            open_file = open(file, "r", encoding="UTF-8")
+            file_text = open_file.read()
+            self.scrape_address(file_text)
+            self.scrape_phone(file_text)
+            self.scrape_BTW(file_text)
+            self.scrape_email(file_text)
 
         self.save_to_json()
 
     def scrape_address(self, file):
-        print(f"Adress is gescraped! from {file}")
+        print(f"Adress is gescraped! from")
 
     def scrape_phone(self, file):
-        print(f"Telefoon nummer is gescraped! from {file}")
+        print(f"Telefoon nummer is gescraped! from")
 
     def scrape_BTW(self, file):
-        print(f"BTW Nummer is gescraped! from {file}")
+        print(f"BTW Nummer is gescraped! from")
 
     def scrape_email(self, file):
-        print(f"Email Adress is gescraped! from {file}")
+        print(f"Email Adress is gescraped! from")
 
     def save_to_json(self):
         return
--- src/info_scraper/main.py
+++ src/info_scraper/main.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 import os
-import glob as glob
 from info_scraper import InfoScraper
-
+from timeit import timeit
 
 
 
@@ -10,26 +9,24 @@ def RunMainLoop():
     main_dir = Path(__file__).parents[2] #Get the folder 3 folders up,
     data_dir = main_dir / 'data' / 'test_data' #Add the 3 folders up folder with the 'data' and 'test_data' folders
     for file in os.listdir(data_dir):
-        current_dir = os.path.join(data_dir, file)
-        #recursively keep adding the directory names until the last file is no longer a directory
-        while os.path.isdir(current_dir):
-            current_dir = AddFileNameToPath(current_dir, os.listdir(current_dir)[0])
-
-        current_dir = Path(current_dir).parents[0] #The above recursion will always go one file to deep, so this is to reel it in once
+        current_dir = GetDeepestFolder(data_dir, file)
         cached_corporate = InfoScraper(current_dir)
-        for to_be_scraped_file in os.listdir(cached_corporate.working_dir):
-            cached_corporate.run_loops(to_be_scraped_file)
-        print(f"alles is gedaan in {current_dir}")
+        cached_corporate.run_loops()
+        print(f"alles is gedaan in {cached_corporate.working_dir}")
+
+
+#recursively keep adding the directory names until the last file is no longer a directory
+def GetDeepestFolder(path, file_name):
+    current_dir = os.path.join(path, file_name)
+    if os.path.isdir(current_dir):
+        return GetDeepestFolder(current_dir, os.listdir(current_dir)[0])
+    else:
+        # The above recursion will always go one file to deep, so this is to reel it in once
+        return Path(current_dir).parents[0]
 
 
 
-def AddFileNameToPath(path, file_name):
-    return os.path.join(path, file_name)
 
 
 
-
-
-print("Lets Start")
-input()
 RunMainLoop()
