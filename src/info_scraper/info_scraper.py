33bfa0f862f1f116050fdc389a7c108d11c4c265
TonsillarRat6 <caspervanwetten@gmail.com>
2022-05-05 09:41:41 +0100

    Finished off Email Approach 1, started work on Phone approach 1

--- src/info_scraper/info_scraper.py
+++ src/info_scraper/info_scraper.py
@@ -7,7 +7,8 @@ import re
 # logger = logging.getLogger()
 
 class InfoScraper:
-    def __init__(self, working_dir=None, phone_number: list = None, email: list = None, kvk: str = None, adress: list = None, zip_code: list = None):
+    def __init__(self, working_dir=None, phone_number: list = None, email: set = None, kvk: str = None,
+                 adress: list = None, zip_code: list = None):
         self.zip_code = zip_code
         self.adress = adress
         self.kvk = kvk
@@ -24,19 +25,56 @@ class InfoScraper:
             self.scrape_BTW(file_text)
             self.scrape_email(file_text)
 
+        self.clean_email()
         self.save_to_json()
 
     def scrape_address(self, file):
-        print(f"Adress is gescraped! from")
+        return
 
     def scrape_phone(self, file):
-        print(f"Telefoon nummer is gescraped! from")
+
+        return
 
     def scrape_BTW(self, file):
-        print(f"BTW Nummer is gescraped! from")
+        return
 
     def scrape_email(self, file):
-        print(f"Email Adress is gescraped! from")
+        pattern = re.compile(r"""
+                        ([a-zA-Z0-9_.+-]+   # one (or more) sets of all characters which numbers, letters or a subset of punctuations
+                        @                   # needs a @    
+                        [a-zA-Z0-9-]+       # any 
+                        \.
+                        [a-zA-Z0-9-.]+
+                        )""", re.VERBOSE)
+        emails = set(re.findall(pattern, file))
+        if self.email is None:
+            self.email = emails
+        else:
+            self.email.update(emails)
+
+    def clean_email(self):
+        """
+        The used RegEx occasionally grabs the '.' at the end of an email, (while it's signing the end of a sentence),
+        this function removes that dot (as well as providing an easy-to-extend way to further add some email-cleaning)
+        """
+        temp_emails = set()
+        for email in self.email:
+            if email[-1] == ".":
+                temp_emails.add(email[:-1])
+            else:
+                temp_emails.add(email)
+        self.email = temp_emails
+
+    def validate_phone(self):
+        validate_phone_number_pattern = "^\\+?\\d{1,4}?[-.\\s]?\\(?\\d{1,3}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$"
+        temp_phones = set()
+        for phone in self.phone_number:
+            if re.match(validate_phone_number_pattern, phone):
+                temp_phones.add(phone)
+        self.phone_number = temp_phones
+
+
 
     def save_to_json(self):
+        print(self.email, self.phone_number, self.kvk, self.adress, self.zip_code)
         return
--- src/info_scraper/main.py
+++ src/info_scraper/main.py
@@ -4,10 +4,9 @@ from info_scraper import InfoScraper
 from timeit import timeit
 
 
-
 def RunMainLoop():
-    main_dir = Path(__file__).parents[2] #Get the folder 3 folders up,
-    data_dir = main_dir / 'data' / 'test_data' #Add the 3 folders up folder with the 'data' and 'test_data' folders
+    main_dir = Path(__file__).parents[2]  # Get the folder 3 folders up,
+    data_dir = main_dir / 'data' / 'test_data'  # Add the 3 folders up folder with the 'data' and 'test_data' folders
     for file in os.listdir(data_dir):
         current_dir = GetDeepestFolder(data_dir, file)
         cached_corporate = InfoScraper(current_dir)
@@ -15,7 +14,7 @@ def RunMainLoop():
         print(f"alles is gedaan in {cached_corporate.working_dir}")
 
 
-#recursively keep adding the directory names until the last file is no longer a directory
+# recursively keep adding the directory names until the last file is no longer a directory
 def GetDeepestFolder(path, file_name):
     current_dir = os.path.join(path, file_name)
     if os.path.isdir(current_dir):
@@ -23,10 +22,6 @@ def GetDeepestFolder(path, file_name):
     else:
         # The above recursion will always go one file to deep, so this is to reel it in once
         return Path(current_dir).parents[0]
-
-
-
-
 
 
 RunMainLoop()
