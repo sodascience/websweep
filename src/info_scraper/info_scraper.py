c2e09a88e8f7524ec077ff75e7c383ff288037cf
TonsillarRat6 <caspervanwetten@gmail.com>
2022-05-11 09:41:15 +0200

    Finalized finding of phone and fax numbers, almost finalized finding of btw and KVK numbers

--- src/info_scraper/info_scraper.py
+++ src/info_scraper/info_scraper.py
@@ -2,21 +2,26 @@ import logging
 import os
 import re
 import time
+import json
+from datetime import date
+from pathlib import Path
 
 # logging.basicConfig(filename="logs/info_scraper.log", level=logging.INFO)
 # logger = logging.getLogger()
-import timeit
+
 
 
 class InfoScraper:
-    def __init__(self, working_dir=None, phone_number: list = None, email: set = None, kvk: str = None,
-                 adress: list = None, zip_code: list = None):
+    def __init__(self, working_dir=None, phone: set = None, email: set = None, kvk: str = None,
+                 adress: list = list(), zip_code: list = list(), fax: set = None):
         self.zip_code = zip_code
         self.adress = adress
         self.kvk = kvk
         self.email = email
-        self.phone_number = phone_number
+        self.phone = phone
+        self.fax = fax
         self.working_dir = working_dir
+
 
     def run_loops(self):
         for file in os.scandir(self.working_dir):
@@ -26,6 +31,8 @@ class InfoScraper:
             self.scrape_phone(file_text)
             self.scrape_BTW(file_text)
             self.scrape_email(file_text)
+            self.scrape_fax(file_text)
+            self.scrape_kvk(file_text)
             open_file.close()
 
         self.clean_email()
@@ -35,8 +42,44 @@ class InfoScraper:
     def scrape_address(self, file):
         return
 
-    def scrape_phone(self, file):
+    def scrape_kvk(self, file):
+        if self.kvk is None:
+            self.kvk = set()
+        pattern = re.compile(r"""
+                                (KvK Nummer:\s*|KvK-Nummer:\s*|KvK nummer:\s*|KvK-nummer:\s*)((\d){8})
+                                """, re.VERBOSE)
+        result_list = set(re.findall(pattern, file))
+        print(result_list)
+        for item in result_list:
+            self.kvk.add(item[1])
+        return
 
+    def scrape_phone(self, file):
+        if self.phone is None:
+            self.phone = set()
+        pattern = re.compile(r"""
+                                (Tel:\s*|
+                                tel:\s*|
+                                telefoon:\s*|
+                                Telefoon:\s*)
+                                (\+*(\d-*\s?){10,})
+                                """, re.VERBOSE)
+        result_list = set(re.findall(pattern, file))
+        for item in result_list:
+            self.phone.add(item[1])
+        return
+
+    def scrape_fax(self, file):
+        if self.fax is None:
+            self.fax = set()
+        pattern = re.compile(r"""
+                                (Fax:\s|
+                                fax:\s)
+                                (\b(\d-*){10,})
+                                """, re.VERBOSE)
+        result_list = set(re.findall(pattern, file))
+        for item in result_list:
+            self.fax.add(item[1])
         return
 
     def scrape_BTW(self, file):
@@ -46,9 +89,9 @@ class InfoScraper:
         pattern = re.compile(r"""
                         ([a-zA-Z0-9_.+-]+   # one (or more) sets of all characters which numbers, letters or a subset of punctuations
                         @                   # needs a @    
-                        [a-zA-Z0-9-]+       # any 
-                        \.
-                        [a-zA-Z0-9-.]+
+                        [a-zA-Z0-9-]+       # again, grab any number or letter
+                        \.                  # the dot in the email
+                        [a-zA-Z0-9-.]+      # grab any combination of letters/numbers/dots, to ensure we also grab stuff like .co.uk
                         )""", re.VERBOSE)
         emails = set(re.findall(pattern, file))
         if self.email is None:
@@ -72,13 +115,27 @@ class InfoScraper:
     def validate_phone(self):
         validate_phone_number_pattern = "^\\+?\\d{1,4}?[-.\\s]?\\(?\\d{1,3}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$"
         temp_phones = set()
-        for phone in self.phone_number:
+        for phone in self.phone:
             if re.match(validate_phone_number_pattern, phone):
                 temp_phones.add(phone)
-        self.phone_number = temp_phones
+        self.phone = temp_phones
 
 
 
     def save_to_json(self):
-        print(self.email, self.phone_number, self.kvk, self.adress, self.zip_code)
+        # file_path = Path(__file__).parents[2] / 'data' / 'Information'
+        # file_name = '\\' + str(date.today()) + '.json'
+        # saved_json = str(file_path) + file_name
+        # file = open(saved_json, 'a')
+        # to_write_dict = {
+        #     'Emails': list(self.email),
+        #     'Phone Numbers': list(self.phone),
+        #     'KVK Numbers': list(self.kvk),
+        #     'Adresses': list(self.adress),
+        #     'Zip codes': list(self.zip_code)
+        # }
+        # prettify = json.dumps(to_write_dict, indent=4)
+        # file.write(prettify)
+        # file.close()
+        print(f"Email: {self.email}, \nTelefoon: {self.phone}, \nKVK: {self.kvk}, \nAdress: {self.adress, self.zip_code}, \nFax: {self.fax} \n")
         return
--- src/info_scraper/main.py
+++ src/info_scraper/main.py
@@ -2,7 +2,8 @@ from pathlib import Path
 import os
 from info_scraper import InfoScraper
 import time
-import glob
+import json
+from datetime import date
 
 def RunMainLoop():
     start_time = time.perf_counter()
@@ -18,7 +19,10 @@ def RunMainLoop():
         time_dict[folder] = end_time_file - start_time_file
         #print(f"alles is gedaan in {cached_corporate.working_dir}")
     end_time = time.perf_counter()
-    print(f"Total execution time is {end_time - start_time}, met deze spreid {time_dict}")
+    prettify = json.dumps(time_dict, indent=4)
+    SavePerformance(time_dict)
+    print(f"Total execution time is {end_time - start_time}, below is a breakdown formatted as \"folder: execution time\" \n{prettify}")
+
 
 # This evaluates all the folders in the last folder (the website folder) and opens the newest (most recent) folder
 def GetFolder(path):
@@ -28,7 +32,14 @@ def GetFolder(path):
     list_of_files.sort(reverse=True)
     return os.path.join(final_dir, list_of_files[0])
 
+def SavePerformance(performance):
+    prettify = json.dumps(performance, indent=4)
+    file_path = Path(__file__).parents[2] / 'data' / 'performance_data'
+    file_name = '\\' + str(date.today()) + '.json'
+    saved_json = str(file_path) + file_name
+    file = open(saved_json, 'w')
+    file.write(prettify)
+    file.close()
+
 if __name__ == "__main__":
     RunMainLoop()
-
-
