1a0acbdd2a707b5f7c62f25bbad2a5e7c64ce446
Casper van Wetten <c.o.vanwetten@gmail.com>
2022-05-30 11:04:45 +0200

    Added docstrings, removed unnecessary return statements

--- src/cpscraper/extractor/extractor.py
+++ src/cpscraper/extractor/extractor.py
@@ -21,7 +21,7 @@ class Extractor:
         for file in os.scandir(self.working_dir):
             open_file = open(file, "r", encoding="UTF-8")
             file_text = open_file.read()
-            self.scrape_address(file_text)
+            self.scrape_adres(file_text)
             self.scrape_zip(file_text)
             self.scrape_phone(file_text)
             self.scrape_email(file_text)
@@ -41,12 +41,18 @@ class Extractor:
                 )
         
 
-    def scrape_address(self, file):
+    def scrape_adres(self, file):
+        """
+        Scrape the adres from the input file, and add found adres to self.adres in set form
+        """
         if self.adres is None:
             self.adres = set()
-        return
+        
 
     def scrape_zip(self, file):
+        """
+        Scrape the zipcode from the input file, and add found zipcodes to self.zip_code in set form
+        """
         if self.zip_code is None:
             self.zip_code = set()
         pattern = re.compile(r"""
@@ -55,9 +61,11 @@ class Extractor:
         result_list = set(re.findall(pattern, file))
         for item in result_list:
             self.zip_code.add(item)
-        return
 
     def scrape_btw(self, file):
+        """
+        Scrape the BTW number from the input file, and add found BTW Numbers (usually only 1) to self.btw in list form
+        """
         if self.btw is None:
             self.btw = set()
         pattern = re.compile(r"""
@@ -66,9 +74,12 @@ class Extractor:
         result_list = set(re.findall(pattern, file))
         for item in result_list:
             self.btw.add(item[2])
-        return
+        
 
     def scrape_kvk(self, file):
+        """
+        Scrape the KVK number from the input file, and add found KVK number to self.kvk in set form
+        """
         if self.kvk is None:
             self.kvk = set()
         pattern = re.compile(r"""
@@ -78,7 +89,7 @@ class Extractor:
         for item in result_list:
             temp = item[0]+' '+item[2]
             self.kvk.add(temp)
-
+        # Note that there are 2 patterns, one for [kvk] [number], and one for [number] [kvk]
         pattern2 = re.compile(r"""
                                 (\b\d{8})(.{0,4})(kvk|KvK|K.v.K.|k.v.k.)
                                 """, re.VERBOSE)
@@ -86,9 +97,12 @@ class Extractor:
         for item in result_list2:
             temp = item[0]+' '+item[2]
             self.kvk.add(temp)
-        return
+        
 
     def scrape_phone(self, file):
+        """
+        Scrape the phone number from the input file, and add found phone numbers to self.phone in set form
+        """
         if self.phone is None:
             self.phone = set()
         pattern = re.compile(r"""
@@ -97,17 +111,21 @@ class Extractor:
                                 telefoon:\s?|
                                 Telefoon:\s?|
                                 T:\s?|
-                                t:\s:
-                                T\s)
-                                (\+?(\s?\d-*){10,11})
+                                t:\s?|
+                                T\s?)
+                                (\+?(\s?\d-\"*){10,11})
                                 """, re.VERBOSE)
+                                # Phone numbers can be indicated by a variety of different ways, this regex tries to incorporate all of those as a possibillity
         result_list = set(re.findall(pattern, file))
         for item in result_list:
             temp = item[0] + ' ' + item[1]
             self.phone.add(temp)
-        return
+        
 
     def scrape_fax(self, file):
+        """
+        Scrape the fax number from the input file, and add found fax numbers to self.fax in set form
+        """
         if self.fax is None:
             self.fax = set()
         pattern = re.compile(r"""
@@ -120,13 +138,16 @@ class Extractor:
         result_list = set(re.findall(pattern, file))
         for item in result_list:
             self.fax.add(item[1])
-        return
+        
 
     def scrape_email(self, file):
+        """
+        Scrape the Email adress from the input file, and adds the found email adress to self.email in set form
+        """
         pattern = re.compile(r"""
                         ([a-zA-Z0-9_.+-]+   # one (or more) sets of all characters which numbers, letters or a subset of punctuations
                         @                   # needs a @    
-                        [a-zA-Z0-9-]{1,20}   # again, grab any number or letter
+                        [a-zA-Z0-9-]{1,20}  # again, grab any number or letter
                         \.                  # the dot in the email
                         (?!png)(?!jpg)[a-zA-Z-.]{1,8}     # grab any combination of letters/numbers/dots, to ensure we also grab stuff like .co.uk
                         )""", re.VERBOSE)
@@ -138,8 +159,7 @@ class Extractor:
 
     def clean_email(self):
         """
-        The used RegEx occasionally grabs the '.' at the end of an email, (while it's signing the end of a sentence),
-        this function removes that dot (as well as providing an easy-to-extend way to further add some email-cleaning)
+        Cleans the self.emails list, as Emails occasionally have an extra '.' at the end of the email adress.
         """
         temp_emails = set()
         for email in self.email:
