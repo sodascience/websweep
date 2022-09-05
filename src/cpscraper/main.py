a00d3bdf2a78b5e9e68ce3c0a2bfa231a0a717f8
Björn Koemans <bjornkoemans2@live.nl>
2022-09-05 17:35:41 +0200

    Implement config command
    using the CLI options you can immediately change the source file, target folder and preferences such as deleting or keeping processed raw files after extraction using `cpscraper config --delete-processed-files` or `cpscraper config --target-folder-path C://Test/Test`

--- src/cpscraper/main.py
+++ src/cpscraper/main.py
@@ -76,7 +76,6 @@ def _get_worker() -> Scraper:
         raise typer.Exit(1)
 
 
-# Init command
 @app.command(name = "init")
 def init() -> None:
     """
@@ -180,18 +179,22 @@ def scrape(config_file) -> None:
     print(f"Downloaded {count} pages from {len(urls)} urls to level {3} in {time() - start:2.1f} seconds.")
 
 
-@app.callback()
-def main(
-    version: Optional[bool] = typer.Option(
-        None,
-        "--version",
-        "-v",
-        help="Show the application's version and exit.",
-        callback=_version_callback,
-        is_eager=True,
-    )
-) -> None:
-    return
+@app.command(name="config")
+def config(
+    delete_processed_files: bool = typer.Option(False, help="Delete / Not-Delete extractor processed raw files"),
+    target_folder_path: str = typer.Option(None, "--target-folder-path", help="Set new path for scraped data output"), 
+    source_file_path: str = typer.Option(None, "--source-file-path", help="Set new path for csv source file")) -> None:
+    if delete_processed_files:
+        print("delete")
+    else:
+        print("save")
+    print(target_folder_path)
+    print(source_file_path)
+
+    if target_folder_path is not None:
+        config._save_target_folder(target_folder_path)
+    if source_file_path is not None:
+        config._save_source_file(source_file_path)
 
 
 @app.command()
@@ -255,6 +258,19 @@ def extract() -> None:
     time_dict["total runtime: "] = total_runtime
 
 
+
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
 
 
 
