5d207e92937e9df06de19ff29b61942a678cad99
Björn Koemans <bjornkoemans2@live.nl>
2023-03-16 10:03:19 +0100

    Update conf.py

--- src/cpscraper/docs/conf.py
+++ src/cpscraper/docs/conf.py
@@ -8,7 +8,7 @@
 
 import os 
 import sys
-sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0,os.path.abspath("."))
 
 project = 'Corporate Scraper'
 copyright = '2023, FIRMBACKBONE'
@@ -22,7 +22,6 @@ extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 
 # -- Options for HTML output -------------------------------------------------
